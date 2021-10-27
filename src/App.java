33cf0680f1bc6b89de843994d7635537226f5fa7
Zachary Dowd <zacharyd2796@zohomail.com>
2021-10-27 11:32:07 -0600

    basic tutorial start

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) {
+    int myNumber;
+    
+    short myShort = 847;
+    long myLong = 10000000;
+    double myDouble = 11.42;
+    char myChar = 'a';
+    boolean myBool = true;
+    byte myByte = 124;
+    myNumber = 100;
+    
+    System.out.println(myNumber);
+    System.out.println(myShort);
+    System.out.println(myLong);
+    System.out.println(myDouble);
+    System.out.println(myChar);
+    System.out.println(myBool);
+    System.out.println(myByte);
+    
+    }
+}

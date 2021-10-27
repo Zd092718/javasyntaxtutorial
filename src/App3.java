873520c9e16d7b331009a4eb7e0b487dd2d15afd
Zachary Dowd <zacharyd2796@zohomail.com>
2021-10-27 11:55:14 -0600

    more tutorials in place

---
+++ src/App3.java
@@ -0,0 +1,13 @@
+public class App3 {
+    public static void main(String[] args) {
+        int value = 0;
+
+        Boolean loop = value > 5;
+
+        while (value <= 20) {
+            System.out.println("Hello " + value);
+
+            value = value + 1;
+        }
+    }
+}

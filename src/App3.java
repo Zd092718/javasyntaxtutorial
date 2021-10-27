02239949c614db62c995163b8deb81104451afcd
Zachary Dowd <zacharyd2796@zohomail.com>
2021-10-27 12:57:19 -0600

    app3 is user input tutorial

--- src/App3.java
+++ src/App3.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+// User input tutorial
 public class App3 {
     public static void main(String[] args) {
 
+        // Create scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Output the prompt
+        System.out.println("Enter a floating point value: ");
+
+        // Wait for user input
+        double value = input.nextDouble();
+
+        // Tell them what they entered
+        System.out.println("You entered: " + value);
     }
 }
